060cc272897a7093d2078adba42e7e23d59f293f
Bikki Hari krishna <bikkiharikrishna@gmail.com>
2020-12-04 15:47:16 +1100

    Version 0.2
    Added Cell
    Connected to cell
    Added collection view extension

---
+++ Picsum/PicsumCell.swift
@@ -0,0 +1,21 @@
+//
+//  PicsumCell.swift
+//  Picsum
+//
+//  Created by Hari Krishna Bikki on 4/12/20.
+//
+
+import UIKit
+
+class PicsumCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    func configure(){
+        
+    }
+    
+    
+}
--- Picsum/ViewController.swift
+++ Picsum/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        NetworkingService.shared.getImgurs { (response) in
+//            self.imgurs = response.imgurs
+//            self.collectionView.reloadData()
+//        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //imageCache.removeAllObjects()
+        
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PicsumCell", for: indexPath) as? PicsumCell else { return UICollectionViewCell() }
+        cell.configure()
+        return cell
     }
 
 
 }
-
